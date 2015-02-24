e77174dd2cbca878c5a6fac68ec1af969e6c9564
Sticklord <epostspamm@gmail.com>
2015-02-24 21:09:27 +0100

    Added fonts

--- Tools/animations.lua
+++ Tools/animations.lua
@@ -138,75 +138,76 @@ local SlideManager = CreateFrame("Frame")
 local SLIDEFRAMES = {}
 local RETURNFRAMES = {}
 
+local START_POSITION = 0
+local ANIMATING_OUT = 1
+local END_POSITION = 2
+local ANIMATING_IN = 3
+
 local function SlideManager_OnUpdate(self, elapsed)
 	local i = 1;
 	local frame, slideInfo;
 	while SLIDEFRAMES[i] do
 		frame = SLIDEFRAMES[i]
 		slideInfo = SLIDEFRAMES[i].slideInfo
-
-		if not slideInfo.slideTimer then
-			slideInfo.slideTimer = 0;
-		end
 		slideInfo.slideTimer = slideInfo.slideTimer + elapsed;
-
-		local p, rp, x, y = unpack(slideInfo.origPos)
-		
-		local distance 
-		if slideInfo.stage == "SlidingBack" then
-			distance = -slideInfo.distance
-		else
-			distance = slideInfo.distance
-		end
-
 		-- delay, lets wait
 		if slideInfo.delay then
 			if slideInfo.delay > slideInfo.slideTimer then
 				slideInfo.delay = slideInfo.delay - elapsed
 			else
 				-- reset the timer
-				slideInfo.delay = nil;
-				slideInfo.slideTimer = 0;
-				slideInfo.stage = "SlidingBack";
+				slideInfo.delay = nil
+				slideInfo.slideTimer = 0
+				slideInfo.stage = ANIMATING_IN
 			end
-		-- Sliding isnt done yet
-		elseif slideInfo.slideTimer < slideInfo.timeToSlide then
-			local yPos, xPos
-			if slideInfo.dir == 'Y' then
-				if slideInfo.stage == "SlidingBack" then
-					yPos = (slideInfo.slideTimer/slideInfo.timeToSlide)^2 * (distance) + (y - distance);
-				else
-					yPos = sqrt(slideInfo.slideTimer/slideInfo.timeToSlide) * (distance) + y;
-				end
-				frame:SetPoint(p, frame:GetParent(), rp, x, yPos)
-			elseif slideInfo.dir == 'X' then
-				if slideInfo.stage == "SlidingBack" then
-					xPos = (slideInfo.slideTimer/slideInfo.timeToSlide)^2 * (distance) + (x - distance);
-				else
-					xPos = sqrt(slideInfo.slideTimer/slideInfo.timeToSlide) * (distance) + x;
-				end
-				frame:SetPoint(p, frame:GetParent(), rp, xPos, y)
-			end
-		-- Were finished
 		else
-			if slideInfo.stage == "SlidingBack" then -- Put it back in original position
-				frame:SetPoint(p, frame:GetParent(), rp, x, y);
-				slideInfo.stage = "Finished"
-			elseif slideInfo.dir == 'Y' then
-				slideInfo.stage = "SlidedOut"
-				frame:SetPoint(p, frame:GetParent(), rp, x, (y + distance));
-			elseif slideInfo.dir == 'X' then
-				slideInfo.stage = "SlidedOut"
-				frame:SetPoint(p, frame:GetParent(), rp, (x + distance), y);
+			local distance 
+			if slideInfo.stage == ANIMATING_IN then
+				distance = -slideInfo.distance
+			else
+				distance = slideInfo.distance
 			end
-			-- Run the finish function if there is one
-			if slideInfo.endFunc then
-				slideInfo.endFunc(slideInfo.arg1, slideInfo.arg2)
-				slideInfo.endFunc, slideInfo.arg1, slideInfo.arg2 = nil, nil, nil;
+			local p, a, rp, x, y = unpack(slideInfo.origPos)
+
+			-- Sliding isnt done yet
+			if slideInfo.slideTimer < slideInfo.timeToSlide then
+				local yPos, xPos
+				if slideInfo.dir == 'Y' then
+					if slideInfo.stage == ANIMATING_IN then
+						yPos = (slideInfo.slideTimer/slideInfo.timeToSlide)^2 * (distance) + (y - distance);
+					else
+						yPos = sqrt(slideInfo.slideTimer/slideInfo.timeToSlide) * (distance) + y;
+					end
+					frame:SetPoint(p, a, rp, x, yPos)
+				elseif slideInfo.dir == 'X' then
+					if slideInfo.stage == ANIMATING_IN then
+						xPos = (slideInfo.slideTimer/slideInfo.timeToSlide)^2 * (distance) + (x - distance);
+					else
+						xPos = sqrt(slideInfo.slideTimer/slideInfo.timeToSlide) * (distance) + x;
+					end
+					frame:SetPoint(p, a, rp, xPos, y)
+				end
+			-- Were finished
+			else
+				if slideInfo.stage == ANIMATING_IN then -- Put it back in original position
+					frame:SetPoint(p, a, rp, x, y);
+					slideInfo.stage = START_POSITION
+				elseif slideInfo.dir == 'Y' then
+					slideInfo.stage = END_POSITION
+					frame:SetPoint(p, a, rp, x, (y + distance));
+				elseif slideInfo.dir == 'X' then
+					slideInfo.stage = END_POSITION
+					frame:SetPoint(p, a, rp, (x + distance), y);
+				end
+				-- Run the finish function if there is one
+				if slideInfo.endFunc then
+					slideInfo.endFunc(slideInfo.arg1, slideInfo.arg2)
+					slideInfo.endFunc, slideInfo.arg1, slideInfo.arg2 = nil, nil, nil;
+				end
+				slideInfo.slideTimer = 0;
+				-- Finished, remove it from the cache
+				tDeleteItem(SLIDEFRAMES, frame);
 			end
-			slideInfo.slideTimer = 0;
-			-- Finished, remove it from the cache
-			tDeleteItem(SLIDEFRAMES, frame);
 		end
 		i = i + 1
 	end
@@ -222,11 +223,11 @@ local function AnimationSlideStart(frame, func, arg1, arg2)
 	while SLIDEFRAMES[index] do
 		-- If frame is already set to sliding 
 		if ( SLIDEFRAMES[index] == frame ) then
-			if frame.slideInfo.stage == "Delay" then
+			if frame.slideInfo.stage == END_POSITION then
 				-- If its waiting to go back, cancel it
-				tDeleteItem(SLIDEFRAMES, frame);
-				frame.slideInfo.stage = "SlidedOut";
-				frame.slideInfo.slideTimer = 0;
+				tDeleteItem(SLIDEFRAMES, frame)
+				frame.slideInfo.stage = END_POSITION
+				frame.slideInfo.slideTimer = 0
 				tinsert(RETURNFRAMES, frame)
 			end
 			return;
@@ -246,7 +247,7 @@ local function AnimationSlideStart(frame, func, arg1, arg2)
 	frame.slideInfo.endFunc = func;
 	frame.slideInfo.arg1 = arg1;
 	frame.slideInfo.arg2 = arg2;
-	frame.slideInfo.stage = "SlidingOut";
+	frame.slideInfo.stage = ANIMATING_OUT;
 
 	tinsert(RETURNFRAMES, frame);
 	tinsert(SLIDEFRAMES, frame);
@@ -275,11 +276,11 @@ local function AnimationSlideReturn(frame, delay)
 	while RETURNFRAMES[index] do
 		-- Found the frame!
 		if ( RETURNFRAMES[index] == frame ) and frame.slideInfo then
-			frame.slideInfo.stage = delay and "Delay" or "SlidingBack";
-			frame.slideInfo.delay = delay;
-			tinsert(SLIDEFRAMES, frame);
+			frame.slideInfo.stage = delay and END_POSITION or ANIMATING_IN
+			frame.slideInfo.delay = delay
+			tinsert(SLIDEFRAMES, frame)
 			tDeleteItem(RETURNFRAMES, frame)
-			SlideManager:SetScript("OnUpdate", SlideManager_OnUpdate);
+			SlideManager:SetScript("OnUpdate", SlideManager_OnUpdate)
 			return;
 		end
 		index = index + 1;
@@ -295,14 +296,14 @@ function ns.SetupFrameForSliding(frame, timeToSlide, horOrVert, distToSlide)
 		horOrVert = "Y";
 	end
 
-	local slideInfo = { };
-	local p, _, rp, x, y = frame:GetPoint()
-	slideInfo.origPos = {p, rp, x, y}; --p, rp, x, y
-	slideInfo.dir = horOrVert;
-	slideInfo.timeToSlide = timeToSlide;
-	slideInfo.distance = distToSlide;
-	slideInfo.stage = "Finished";
-	frame.slideInfo = slideInfo;
+	local slideInfo = frame.slideInfo and wipe(frame.slideInfo) or { };
+	slideInfo.origPos = {frame:GetPoint()}
+	slideInfo.dir = horOrVert
+	slideInfo.timeToSlide = timeToSlide
+	slideInfo.distance = distToSlide
+	slideInfo.stage = START_POSITION
+	slideInfo.slideTimer = 0
+	frame.slideInfo = slideInfo
 
 	frame.AnimationSlideReturn = AnimationSlideReturn
 	frame.AnimationSlideStart = AnimationSlideStart
